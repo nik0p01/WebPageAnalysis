02c88dcf006ae03c476710c277ec8d5b1ae691ef
Николай Симонов <simnikola@gmail.com>
2023-05-21 13:11:12 +0300

    edited output format

--- WebPageAnalysis/Controllers/WordCounterController.cs
+++ WebPageAnalysis/Controllers/WordCounterController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using WebPageAnalysis.DAL.Entities;
 using WebPageAnalysis.DAL.Repository;
 using WebPageAnalysis.Services;
 
@@ -12,7 +11,7 @@ namespace WebPageAnalysis.Controllers
         private readonly ILogger<WordCounterController> _logger;
         private readonly IWordCountRepository _wordCountRepository;
         private readonly IWebPageWorker _worker;
-        
+
         public WordCounterController(IWordCountRepository wordCountRepository, ILogger<WordCounterController> logger, IWebPageWorker worker)
         {
             _wordCountRepository = wordCountRepository;
@@ -21,13 +20,14 @@ namespace WebPageAnalysis.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<WordCount>> GetAsync(CancellationToken cancellationToken)
+        public async Task<IDictionary<string, int>> GetAsync(CancellationToken cancellationToken)
         {
+
             var word = ".jpg";
             string[] urls = { @"https://regnum.ru/foreign/balkans/serbia/belgrad", @"https://regnum.ru/foreign/balkans/serbia/belgrad", @"https://regnum.ru/foreign/balkans/serbia/belgrad" };
             var count = await _worker.CouuntWordAsync(urls, word, cancellationToken);
             await _wordCountRepository.UpdateAsync(word, count, cancellationToken);
-            return await _wordCountRepository.GetAsync(cancellationToken);
+            return (await _wordCountRepository.GetAsync(cancellationToken)).ToDictionary(keySelector: w => w.Word, elementSelector: w => w.Count);
         }
     }
 }
